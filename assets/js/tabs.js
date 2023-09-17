4e21cefac5b71647406ac3c1a5633ef73365ae1d
Nordegraf <Nordegraf@users.noreply.github.com>
2023-09-17 22:59:10 +0000

    deploy: 3d8aad35775dd11983e28687297b92f669dc7c1b

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,13 +1,32 @@
 document.querySelectorAll('button[data-bs-toggle="tab"]').forEach((t,i)=>{
     t.addEventListener('show.bs.tab', function (e) {
         let targetClass = t.dataset.bsTarget
-        var pane = document.querySelector('#SetupTabB '+targetClass)
-        var sibling = document.querySelector('#SetupTabB .tab-pane.active')
-        // hide 2nd pane sibling
-        sibling.classList.remove('show')
-        sibling.classList.remove('active')
-        // show 2nd pane
-        pane.classList.add('show')
-        pane.classList.add('active')
+
+        // panes to be shown
+        var hiddenPanes = document.querySelectorAll('.tab-pane' + targetClass)
+        // currently shown panes
+        var activePanes = document.querySelectorAll('.tab-pane.active')
+
+        // hide currently shown panes
+        activePanes.forEach((pane, i) => {
+            pane.classList.remove('show')
+            pane.classList.remove('active')
+        })
+
+        // show panes to be shown
+        hiddenPanes.forEach((pane, i) => {
+            pane.classList.add('show')
+            pane.classList.add('active')
+        })
+
+        // switch $HOME and %HOMEPATH% in homepath divs
+        var homepathDivs = document.querySelectorAll('.homepath')
+        homepathDivs.forEach((div, i) => {
+            if (div.innerHTML == '$HOME') {
+                div.innerHTML = '%HOMEPATH%'
+            } else {
+                div.innerHTML = '$HOME'
+            }
+        })
     })
 })
